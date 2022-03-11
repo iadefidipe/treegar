b7d957aeed4815511c0ac81f39840efc2aa31b45
Israel Adefidipe <israeladefidipe@gmail.com>
2022-03-11 10:22:16 +0100

    Feat: Project intial commit

---
+++ components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+function Layout() {
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
---
+++ data/theme.js
@@ -0,0 +1,49 @@
+const theme = {
+    colors: {
+      pryBlack: "#181818",
+      bg: "#040404",
+      accent: "#4CC800",
+      pryWhite: "#FFFFFF",
+      secGrey: "#828282",
+      secLightGrey: "#BDBDBD",
+      secAccent: "#CBFFCA",
+      shade: "#A7F9AF"
+    },
+    font: {
+      redHat: `'Red Hat Text', sans-serif`,
+    },
+    weight: {
+      regular: "400",
+      mid: "500",
+      semiBold: "600",
+      Bold:"700"
+    },
+    queries: {
+      mobile: `${375 / 16}rem`,
+      mobileL: `${425 / 16}rem`,
+      mobileXL: `${545 / 16}rem`,
+      tabletMid: `${660 / 16}rem`,
+      tablet: `${768 / 16}rem`,
+      deskMini: `${976 / 16}rem`,
+      deskMid: `${1129 / 16}rem`,
+      deskMed: `${1300 / 16}rem`,
+    },
+    bg: {
+      ctaBg,
+      twoCircle,
+      threeCircle,
+      leaf,
+      smCircle,
+      introMobile,
+      introTablet,
+      bgHome,
+      leafLeft,
+      bgAboutHero,
+    },
+    bRadius: "15px",
+    maxWidth: "1111px",
+    genPad: "0 20px",
+    center: "0 auto",
+  }
+  
+  export default theme
