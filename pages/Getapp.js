9dcae3350372cd1e861a14bd78ea7bee092071c5
Israel <israeladefidipe@gmail.com>
2022-03-19 23:12:45 +0100

    fix: Getapp page reviewed

--- pages/Getapp.js
+++ pages/Getapp.js
@@ -1,36 +1,48 @@
 import styled from "styled-components"
 import { Heading1, Heading2 } from "../components/shared/Headings"
-import { fontRegular } from "../components/shared/typography"
 import { IconButton } from "../components/shared/Buttons"
-import Head from 'next/head'
-
+import Head from "next/head"
+import { maxWidth, InnerWrapper } from "../components/shared/BaseStyles"
 
 const Wrapper = styled.main`
-  text-align: center;
-  display: grid;
-  gap: 50px;
-  margin-top: 150px;
-  padding: 100px 0;
-
+  ${maxWidth}
+  height: 100vh;
   @media (min-width: 768px) {
     background: ${(props) =>
-      `url(${props.theme.bg.bgTree.src}) no-repeat right 380px`};
+      `url(${props.theme.bg.bgTree.src}) no-repeat right 750px`};
   }
 `
 
 const MainText = styled(Heading1)``
 const SecText = styled(Heading2)`
-  font-weight: ${(props) => props.theme.weight.bold};
+  margin: 0 auto;
+  max-width: 400px;
+  font-size: 1.6875rem;
+
   line-height: 31px;
   & > span {
     color: ${(props) => props.theme.colors.accent};
   }
+
+  @media (min-width: 768px) {
+    font-size: 2.5rem;
+    line-height: 60px;
+  }
 `
-const PlainText = styled.p`
+const PageContainer = styled(InnerWrapper)`
+  text-align: center;
   display: grid;
   gap: 50px;
+  margin-top: 150px;
+  padding: 100px 0;
+`
+const PlainText = styled.p`
+  font-size: 1rem;
 
-  ${fontRegular}
+  opacity: 0.8;
+  @media (min-width: 768px) {
+    font-size: 1.25rem;
+  }
 `
 const ButtonWrap = styled.div`
   display: flex;
@@ -46,23 +58,24 @@ const Button = styled(IconButton)``
 function Getapp() {
   return (
     <>
-    <Head>
-      <title> Treegar - Get App</title>
-    </Head>
-    <Wrapper>
-      <MainText>Hey there!👋</MainText>
-      <TextWrap>
-        <SecText>
-          Get started with <br /> <span>treegar</span> in seconds
-        </SecText>
-        <PlainText>Tap on an icon to get started</PlainText>
-      </TextWrap>
-      <ButtonWrap>
-        <Button play />
-
-        <Button />
-      </ButtonWrap>
-    </Wrapper>
+      <Head>
+        <title> Treegar - Get App</title>
+      </Head>
+      <Wrapper>
+        <PageContainer>
+          <MainText>Hey there!👋</MainText>
+          <TextWrap>
+            <SecText>
+              Get started with <span>treegar</span> in seconds
+            </SecText>
+            <PlainText>Tap on an icon to get started</PlainText>
+          </TextWrap>
+          <ButtonWrap>
+            <Button play />
+            <Button />
+          </ButtonWrap>
+        </PageContainer>
+      </Wrapper>
     </>
   )
 }
